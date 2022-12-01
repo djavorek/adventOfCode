3090d21b31a0490c83c138d6df8109d4b714fd7e
Denes Javorek <javorek.denes@gmail.com>
2022-12-01 17:45:15 +0100

    Finished Day 1 part 2

--- 2022/aoc/src/day/day_01.rs
+++ 2022/aoc/src/day/day_01.rs
@@ -1,7 +1,7 @@
 use crate::util::{vec_of_strings};
 
 use super::Day;
-use itertools::max;
+use itertools::{max, Itertools};
 
 static DATE: u8 = 1;
 pub struct Day01;
@@ -13,22 +13,28 @@ impl Day<u32> for Day01 {
 
     fn solve_part_1(&self, input: &str) -> u32 {
         let lines = vec_of_strings(input);
-        let mut current = 0;
-        let mut sums: Vec<u32> = vec![];
-
-        for line in lines {
-            if line.is_empty() {
-                sums.push(current);
-                current = 0;
-            } else {
-                current += line.parse::<u32>().unwrap();
-            }
-        }
-
-        sums.push(current);
-        return max(sums).unwrap();
+        return max(calculate_calories_sums(lines)).unwrap();
     }
+
     fn solve_part_2(&self, input: &str) -> u32 {
-        2
+        let lines = vec_of_strings(input);
+        let sums = calculate_calories_sums(lines);
+
+        return sums.iter().sorted().rev().take(3).sum();
     }
+}
+
+fn calculate_calories_sums(lines: Vec<&str>) -> Vec<u32> {
+    let mut current = 0;
+    let mut sums: Vec<u32> = vec![];
+    for line in lines {
+        if line.is_empty() {
+            sums.push(current);
+            current = 0;
+        } else {
+            current += line.parse::<u32>().unwrap();
+        }
+    }
+    sums.push(current);
+    sums
 }
