4957e3895f4924aeb448bfce6a469e6404789d38
Denes Javorek <javorek.denes@gmail.com>
2022-12-02 13:06:29 +0100

    Refactor before part 2

--- 2022/aoc/src/day/day_02.rs
+++ 2022/aoc/src/day/day_02.rs
@@ -13,62 +13,80 @@ impl Day<u32> for Day02 {
     }
 
     fn solve_part_1(&self, input: &str) -> u32 {
+        let part_1_decryption: HashMap<char, Meaning> = HashMap::from([
+            ('A', Meaning::Rock),
+            ('B', Meaning::Paper),
+            ('C', Meaning::Scissors),
+            ('X', Meaning::Rock),
+            ('Y', Meaning::Paper),
+            ('Z', Meaning::Scissors),
+        ]);
+
         let input_lines = vec_of_strings(input);
-        let plays_by_rounds: Vec<(&str, &str)> = input_lines.iter()
+
+        return input_lines.iter()
             .map(|line| line.split_once(" ").unwrap())
-            .collect();
-
-        let score = plays_by_rounds.iter()
-            .map(|touple| calculate_round_score(touple.0, touple.1))
+            .map(|touple| {
+                let step = find_step_according_to_map(touple.0, part_1_decryption.to_owned());
+                let own_step = find_step_according_to_map(touple.1, part_1_decryption.to_owned());
+                calculate_total_round_score(step, own_step)
+            } )
             .fold(0, |acc, score| acc + score as u32);
-
-        score
     }
 
     fn solve_part_2(&self, input: &str) -> u32 {
-        2
+        let part_2_decryption: HashMap<char, Meaning> = HashMap::from([
+            ('A', Meaning::Rock),
+            ('B', Meaning::Paper),
+            ('C', Meaning::Scissors),
+            ('X', Meaning::Lose),
+            ('Y', Meaning::Draw),
+            ('Z', Meaning::Win),
+        ]);
+
+        let input_lines = vec_of_strings(input);
+
+        return input_lines.iter()
+            .map(|line| line.split_once(" ").unwrap())
+            .map(|touple| {
+                let step = find_step_according_to_map(touple.0, part_2_decryption.to_owned());
+                let own_step = find_step_according_to_map(touple.1, part_2_decryption.to_owned());
+                calculate_total_round_score(step, own_step)
+            } )
+            .fold(0, |acc, score| acc + score as u32);
     }
 }
 
-fn calculate_round_score(step: &str, own_step: &str) -> u8 {
-    let char_map: HashMap<char, Step> = HashMap::from([
-        ('A', Step::Rock),
-        ('B', Step::Paper),
-        ('C', Step::Scissors),
-        ('X', Step::Rock),
-        ('Y', Step::Paper),
-        ('Z', Step::Scissors),
-    ]);
-
-    let step_char = step.chars().next().expect("Empty Elf-step found in input");
-    let own_step_char = own_step.chars().next().expect("Empty Own-step found in input");
-    
-    let step_value = char_map.get(&step_char).expect("Unknown Elf-step found in input");
-    let own_step_value = char_map.get(&own_step_char).expect("Unknown Own-step found in input");
-
-    let round_result_score = get_round_result_score(*step_value, *own_step_value);
-    
-
-    round_result_score + *own_step_value as u8
+fn calculate_total_round_score(step: Meaning, own_step: Meaning) -> u8 {
+    let round_result_score = calculate_round_result_score(step, own_step);
+    round_result_score + own_step as u8
 }
 
-fn get_round_result_score(step: Step, own_step: Step) -> u8 {
+fn calculate_round_result_score(step: Meaning, own_step: Meaning) -> u8 {
     if step == own_step {
         return 3;
     }
 
-    if (own_step == Step::Rock && step == Step::Scissors) ||
-       (own_step == Step::Paper && step == Step::Rock) || 
-       (own_step == Step::Scissors && step == Step::Paper) {
+    if (own_step == Meaning::Rock && step == Meaning::Scissors) ||
+       (own_step == Meaning::Paper && step == Meaning::Rock) || 
+       (own_step == Meaning::Scissors && step == Meaning::Paper) {
         return 6;
     }
 
     0
 }
 
+fn find_step_according_to_map(input: &str, meaning_map: HashMap<char, Meaning>) -> Meaning {
+    let step_char = input.chars().next().expect("Input is lazy, eww.");
+    return *meaning_map.get(&step_char).expect("Input is playing by its own rules, JEEZ");
+}
+
 #[derive(Copy, Clone, PartialEq)]
-enum Step {
+enum Meaning {
     Rock = 1,
     Paper = 2,
     Scissors = 3,
+    Win,
+    Lose,
+    Draw,
 }
