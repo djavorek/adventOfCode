b503a9fc261cd1b2f3cfcdc3b5eea3af31449877
Denes Javorek <javorek.denes@gmail.com>
2023-09-18 21:39:29 +0200

    2022 Day 9 Part 1 + logging complete

--- 2022/aoc/src/day/day_09.rs
+++ 2022/aoc/src/day/day_09.rs
@@ -7,12 +7,15 @@ use super::Day;
 pub struct Day09;
 static DATE: u8 = 9;
 
+const PRINT_LIMIT: i8 = 15;
+
 impl Day<u32> for Day09 {
     fn get_date(&self) -> u8 {
         DATE
     }
 
     fn solve_part_1(&self, input: &str) -> u32 {
+        // let mut print_value = 0;
         let input_lines = vec_of_strings(input);
 
         let mut end_positions = { EndPositions {
@@ -22,23 +25,69 @@ impl Day<u32> for Day09 {
         let mut tail_positions: HashSet<Position> = HashSet::new();
         tail_positions.insert(end_positions.tail);
 
+        // println!("- Start");
+        // print_result(&end_positions);
+
         for instruction in input_lines {
-            end_positions.head = find_new_head(instruction, &end_positions.head);
-            end_positions.tail = find_new_tail(&end_positions);
+            let (dir, count) = parse_instruction(instruction);
+            // if print_value < PRINT_LIMIT {
+            //     println!("\n Instruction -- {}", instruction);
+            // }
 
-            tail_positions.insert(end_positions.tail);
+            for _step in 0..count {
+                end_positions.head = find_new_head(dir, &end_positions.head);
+                end_positions.tail = find_new_tail(&end_positions);
 
+                tail_positions.insert(end_positions.tail);
+
+                // if print_value < PRINT_LIMIT {
+                //     print_result(&end_positions);
+                //     print_value += 1;
+                // }
+            }
         }
 
         tail_positions.len() as u32
     }
 
-    fn solve_part_2(&self, _input: &str) -> u32 {
-      201600
+    fn solve_part_2(&self, input: &str) -> u32 {
+        let mut print_value = 0;
+        let input_lines = vec_of_strings(input);
+
+        let mut end_positions = { EndPositions {
+            head: Position::default(),
+            tail: Position::default()
+        }};
+        let mut tail_positions: HashSet<Position> = HashSet::new();
+        tail_positions.insert(end_positions.tail);
+
+        println!("- Start");
+        print_result(&end_positions);
+
+        for instruction in input_lines {
+            let (dir, count) = parse_instruction(instruction);
+            if print_value < PRINT_LIMIT {
+                println!("\n Instruction -- {}", instruction);
+            }
+
+            for _step in 0..count {
+                end_positions.head = find_new_head(dir, &end_positions.head);
+                end_positions.tail = find_new_tail(&end_positions);
+
+                tail_positions.insert(end_positions.tail);
+
+                if print_value < PRINT_LIMIT {
+                    print_result(&end_positions);
+                    print_value += 1;
+                }
+            }
+        }
+
+        tail_positions.len() as u32
     }
 
     fn get_expected_result_1(&self) -> u32 {
-        1849
+        6256
     }
 
     fn get_expected_result_2(&self) -> u32 {
@@ -57,18 +106,22 @@ pub struct EndPositions {
     tail: Position
 }
 
-fn find_new_head(instruction: &str, current_position: &Position) -> Position {
+fn parse_instruction(instruction: &str) -> (&str, i16) {
     let (dir, step_str) = instruction.split_once(" ").expect("Invalid instruction");
     let step = step_str.trim().parse::<i16>().expect("Invalid step count");
 
+    return (dir, step);
+}
+
+fn find_new_head(dir: &str, current_position: &Position) -> Position {
     let mut new_x = current_position.x;
     let mut new_y = current_position.y;
 
     match dir {
-        "L" => new_x -= step,
-        "R" => new_x += step,
-        "U" => new_y += step,
-        "D" => new_y -= step,
+        "L" => new_x -= 1,
+        "R" => new_x += 1,
+        "U" => new_y -= 1,
+        "D" => new_y += 1,
         _ => println!("Invalid direction")
     }
 
@@ -91,13 +144,31 @@ fn find_new_tail(positions: &EndPositions) -> Position {
 
     let new_y = if y_diff < 1 {
         positions.tail.y
-    } else if (y_diff == 1 && y_diff <= 1) {
+    } else if y_diff == 1 && x_diff <= 1 {
         positions.tail.y
-    } else if (positions.head.y > positions.tail.y) {
+    } else if positions.head.y > positions.tail.y {
         positions.tail.y + 1
     } else {
         positions.tail.y - 1
     };
 
     return Position { x: new_x, y: new_y }
+}
+
+fn print_result(end_positions: &EndPositions) -> () {
+    print!("\n");
+    for y in 0..10 {
+        for x in 0..10 {
+            if end_positions.head.x + 5 == x && end_positions.head.y + 5 == y {
+                print!("H");
+                continue;
+            }
+            if end_positions.tail.x + 5 == x && end_positions.tail.y + 5 == y {
+                print!("T");
+                continue;
+            }
+            print!(".")
+        }
+        print!("\n");
+    }
 }
